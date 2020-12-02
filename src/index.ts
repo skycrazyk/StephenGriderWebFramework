6ea1f101eb319401ced2e6c36e203b02a3edeaf4
Dmitry Karyakin <skycrazyk@gmail.com>
2020-12-02 23:18:37 +0500

    start User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello!')
+import  { User } from './User'
+
+const user = new User({ name:'Ivan', age: 16 })
+
+user.set({ age: 18 })
+
+console.log(user.get('name'))
+console.log(user.get('age'))
