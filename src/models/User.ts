3d58df945abb771b46416b75375fd73fb4e77d6d
Dmitry Karyakin <skycrazyk@gmail.com>
2020-12-13 00:21:42 +0500

    Extract Attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,38 +1,15 @@
-import axios from "axios";
-import { Eventing } from "./Eventing";
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
 
+const rootUrl = 'http://localhost:1234/user';
+
 export class User {
   events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get("id")}`).then((res) => {
-      this.set(res.data);
-    });
-  }
-
-  save(): void {
-    const id = this.get("id");
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${this.get("id")}`, this.data);
-    } else {
-      axios.post("http://localhost:3000/users", this.data);
-    }
-  }
+  sync: Sync<UserProps> = new Sync(rootUrl);
 }
