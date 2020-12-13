9bd75ca100482382c09705b0d565be5870968e74
Dmitry Karyakin <skycrazyk@gmail.com>
2020-12-13 20:38:07 +0500

    integrate attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   name?: string;
@@ -12,4 +13,9 @@ const rootUrl = 'http://localhost:1234/user';
 export class User {
   events: Eventing = new Eventing();
   sync: Sync<UserProps> = new Sync(rootUrl);
+  attrs: Attributes<UserProps>;
+
+  constructor(data: UserProps) {
+    this.attrs = new Attributes(data);
+  }
 }
