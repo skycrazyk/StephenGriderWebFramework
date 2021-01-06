dc72fef6976eb58f74f8a82ee88c1e0a9f458cc1
Dmitry Karyakin <skycrazyk@gmail.com>
2021-01-06 20:59:45 +0500

    added root route

--- Express/src/routes/loginRoutes.ts
+++ Express/src/routes/loginRoutes.ts
@@ -36,4 +36,21 @@ router.post('/login', (req: RequestWithBody, res) => {
   }
 });
 
+router.get('/', (req, res) => {
+  if (req.session?.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
 export { router };
