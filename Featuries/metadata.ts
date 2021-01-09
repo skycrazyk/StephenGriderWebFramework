26f492f29fa1099c0940feb077a38728ee46ab38
Dmitry Karyakin <skycrazyk@gmail.com>
2021-01-09 17:03:31 +0500

    use metadata

--- Featuries/metadata.ts
+++ Featuries/metadata.ts
@@ -11,3 +11,26 @@ console.log(Reflect.getMetadata('size', plane));
 Reflect.defineMetadata('type', 'single color', plane, 'color');
 
 console.log(Reflect.getMetadata('type', plane, 'color'));
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('hi there')
+  fly(): void {
+    console.log('vrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: any, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
